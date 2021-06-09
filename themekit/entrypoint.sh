0805882278094c433c0dc248335de4f68988a2c8
Grant Eagon <grant.eagon@gmail.com>
2021-06-09 02:08:15 -0400

    Update entrypoint.sh

--- themekit/entrypoint.sh
+++ themekit/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-
+ls -al
+echo $SHOPIFY_PASSWORD
+echo $SHOPIFY_STORE_URL
+echo $SHOPIFY_THEME_ID
+echo $THEME_PATH
 theme deploy --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH --allow-live $INPUT_ARGS
